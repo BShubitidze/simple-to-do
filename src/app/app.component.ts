99b0410802f9d4a17dfbfe3d476c9af7345b3f07
BShubitidze <besoshubitidze93@gmail.com>
2026-04-08 20:00:36 +0400

    simple string tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'work-2';
+  tasks = signal<string[]>([]);
+  newTask = signal('');
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.newTask.set(input.value);
+  }
+
+  addTaks() {
+    const title = this.newTask().trim;
+
+    if (!title) return;
+
+    this.tasks.update((t) => [...t, this.newTask()]);
+
+    this.newTask.set('');
+  }
+
+  removeTask(index: number) {
+    this.tasks.update((t) => t.filter((_, i) => i !== index));
+  }
 }
